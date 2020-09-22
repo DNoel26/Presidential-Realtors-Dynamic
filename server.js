ca0136164f284899dfc89626b02016cbc32828bc
DNoel26 <darnellnoel.webdev@gmail.com>
2020-09-22 23:29:27 +0100

    commit 3

--- server.js
+++ server.js
@@ -1,4 +1,10 @@
 const express = require('express');
+require('dotenv').config({path: 'config/keys.env'});
+const exphbs = require('express-handlebars');
+const bcryptjs = require('bcryptjs');
+const session = require('express-session');
+const file_upload = require('express-fileupload');
+const mysql = require('mysql2');
 
 const app = express();
 
